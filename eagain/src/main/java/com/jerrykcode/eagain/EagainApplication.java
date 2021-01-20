885a44586ef22e685ddf770f4929afcc2bd530f8
jerrykcode <jerrykang026@gmail.com>
2021-01-20 14:40:11 +0800

    Initail commit

---
+++ eagain/src/main/java/com/jerrykcode/eagain/EagainApplication.java
@@ -0,0 +1,13 @@
+package com.jerrykcode.eagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagainApplication.class, args);
+	}
+
+}
